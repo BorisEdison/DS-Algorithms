b80e47e52488d2ce67eed29976bd23e960527b32
Boris Edison <67020745+BorisEdison@users.noreply.github.com>
2022-02-18 22:17:32 +0530

    Add files via upload

---
+++ BinarySearch/binarySearch.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int BS(int key, int a[],int min, int max);
+
+int main(){
+	int a[50],n,k;
+	printf("How many elements do you want to insert:\t");
+	scanf("%d",&n);
+	for(int i=0; i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	printf("\nWhich element do you want to search:");
+	scanf("%d",&k);
+	int index = BS(k,a,0,n-1);
+	printf("The element is at %d index", index);
+}
+
+BS(int key, int a[], int min, int max){
+	if(min > max){
+		return -1;
+	}
+	
+	int mid = ((max + min) /2);
+	if(a[mid] == key){
+		return mid;
+	}
+	if(key>a[mid]){
+		BS(key,a,mid+1,max);
+	}
+	else if (key<a[mid]){
+		BS(key,a,min,mid-1);
+	}
+}
+
+
